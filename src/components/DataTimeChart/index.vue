9fad9234847d1a041df5dfe973522d03d0273d19
wshicheng <630030437@qq.com>
2017-10-16 18:35:09 +0800

    modify 18:35

--- src/components/DataTimeChart/index.vue
+++ src/components/DataTimeChart/index.vue
@@ -6,6 +6,7 @@
 var Highcharts = require('highcharts');
 // 在 Highcharts 加载之后加载功能模块
 require('highcharts/modules/exporting')(Highcharts);
+
 export default {
     methods: {
         initChart(title,xAxis,data) {
@@ -36,21 +37,25 @@ export default {
                 yAxis: {
                     min: 0,
                     title: {
-                        text: '降雨量 (mm)'
+                        text: ''
                     }
                 },
-                tooltip: {
-                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-                    footerFormat: '</table>',
-                    shared: true,
-                    useHTML: true
+               tooltip: {
+                   
+                    formatter: function() {  
+                        console.log(this)
+                       return '时长分布:' + this.x + '<br>' + '单数' + ':' + Highcharts.numberFormat(this.point.y, 0,"",",");
+                        
+                    }                 
                 },
                 series: [{
                     name: '东京',
                     data:data
-                }]
+                },
+                ],
+                 legend: {
+                    enabled: false
+                },
             }
 
             new Highcharts.chart('container', options);
---
+++ src/components/DataTimeChart/index2.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="container" style="min-width:400px;height:400px">
+    </div>
+</template>
+<script>
+var Highcharts = require('highcharts');
+// 在 Highcharts 加载之后加载功能模块
+require('highcharts/modules/exporting')(Highcharts);
+
+export default {
+    methods: {
+        initChart(title,xAxis,data) {
+            var options = {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: title
+                },
+                xAxis: {
+                    categories:xAxis
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: '水果消费总量'
+                    },
+                    stackLabels: {
+                        enabled: true,
+                        style: {
+                            fontWeight: 'bold',
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                        }
+                    }
+                },
+                legend: {
+                    align: 'right',
+                    x: -30,
+                    verticalAlign: 'top',
+                    y: 25,
+                    floating: true,
+                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                    borderColor: '#CCC',
+                    borderWidth: 1,
+                    shadow: false
+                },
+                tooltip: {
+                    formatter: function() {
+                        return '<b>' + this.x + '</b><br/>' +
+                            this.series.name + ': ' + this.y + '<br/>' +
+                            '总量: ' + this.point.stackTotal;
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                            style: {
+                                textShadow: '0 0 3px black'
+                            }
+                        }
+                    }
+                },
+                series: data
+            }
+
+            new Highcharts.chart('container', options);
+        },
+    },
+    props: ['title', 'xAxis', 'chartData'],
+    mounted() {
+        this.initChart(this.title,this.xAxis,this.chartData)
+    }
+}
+</script>
