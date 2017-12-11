2b93d9a30b0241c48b7d602915727e975f135f31
wshicheng <630030437@qq.com>
2017-12-11 17:12:46 +0800

    modify

--- src/view/orderData/orderAllData/index.vue
+++ src/view/orderData/orderAllData/index.vue
@@ -625,6 +625,10 @@ export default {
                         this.chartProfitRate.push(Number(item.profitRate))
                         this.chartDataX.push(this.cityType === 1?item.cityName:item.orderTime)
                     })
+                    this.chartDataPayAmount = this.chartDataPayAmount.unique()
+                    this.chartDisCountAmount =  this.chartDisCountAmount.unique()
+                    this.chartProfitRate = this.chartProfitRate.unique()
+                    this.chartDataX = this.chartDataX.unique()
                     this.initChart()
                     this.loadFlag = true
                 }
@@ -909,7 +913,6 @@ export default {
     },
     watch: {
         '$store.state.cityList': 'cityChange',
-        'sortNum': 'sortData'
     }
 }
 </script>
